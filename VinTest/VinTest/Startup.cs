288cc1ee3a6c362baa368d1abaa3b42aaad55857
vinaya.shetty <vinaya.shetty>
2017-05-15 15:40:59 +0530

    Test

---
+++ VinTest/VinTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VinTest.Startup))]
+namespace VinTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
